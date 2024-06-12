f31e9c9e9243b89d2fc244cde264bf8113c8f7e5
SaiyedRushan <rushan52@gmail.com>
2024-06-12 17:45:52 -0400

    create exchangerate model and migrations

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class ExchangeRate(models.Model):
+    base_currency = models.CharField(max_length=3)
+    target_currency = models.CharField(max_length=3)
+    date = models.DateField()
+    rate = models.DecimalField(max_digits=10, decimal_places=4)
+
+    class Meta:
+        unique_together = ('base_currency', 'target_currency', 'date')
+  
