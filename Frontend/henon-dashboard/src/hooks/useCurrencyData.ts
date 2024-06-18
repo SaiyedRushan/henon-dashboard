64cb782300534dbdc89570a5d2d96a5ecd6a6033
SaiyedRushan <rushan52@gmail.com>
2024-06-18 14:49:53 -0400

    fix fetch request url

--- Frontend/henon-dashboard/src/hooks/useCurrencyData.ts
+++ Frontend/henon-dashboard/src/hooks/useCurrencyData.ts
@@ -21,7 +21,7 @@ export default function useCurrencyData(baseCurrency: string, currencies: string
         setData({ rates: {} })
         setLoading(true)
         const targetCurrencies = currencies.filter((currency) => currency !== baseCurrency).join(",")
-        const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/v1/exchange-rates?base_currency=${baseCurrency}&target_currencies=${targetCurrencies}&start_date=${startDate}&end_date=${endDate}`)
+        const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/v1/exchange-rates/?base_currency=${baseCurrency}&target_currencies=${targetCurrencies}&start_date=${startDate}&end_date=${endDate}`)
         const data = response.data
         setData(data)
       } catch (err: any) {
