7cf29031e92de2438f0eafb69027a4c0892adb60
SaiyedRushan <rushan52@gmail.com>
2024-06-24 22:11:10 -0400

    add some transitions to the header and buttons

--- Frontend/henon-dashboard/src/App.tsx
+++ Frontend/henon-dashboard/src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from "react"
 
-import { Box, Button, Container, CssBaseline, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material"
+import { Box, Button, Collapse, Container, CssBaseline, Fade, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material"
 import useCurrencyData from "./hooks/useCurrencyData"
 import LineChart from "./components/LineChart"
 import Grid from "./components/Grid"
@@ -17,6 +17,7 @@ function App() {
   const [baseCurrency, setBaseCurrency] = useState(() => localStorage.getItem("baseCurrency") || "USD")
   const [startDate, setStartDate] = useState(localStorage.getItem("startDate") || twoYearsAgo)
   const [endDate, setEndDate] = useState(localStorage.getItem("endDate") || today)
+  const [fadeIn, setFadeIn] = useState(false)
 
   const { data, loading, error }: { data: any; loading: boolean; error: any } = useCurrencyData(baseCurrency, currencies, startDate, endDate)
   const { toggleColorMode, mode } = useThemeContext()
@@ -34,6 +35,10 @@ function App() {
   }
 
   useEffect(() => {
+    setFadeIn(true)
+  }, [])
+
+  useEffect(() => {
     localStorage.setItem("baseCurrency", baseCurrency)
     localStorage.setItem("startDate", startDate)
     localStorage.setItem("endDate", endDate)
@@ -43,69 +48,75 @@ function App() {
   return (
     <Container>
       <CssBaseline />
-      <Box display={"flex"} justifyContent={"center"}>
-        <h1>Exchange Rates</h1>
-        <Button onClick={toggleColorMode}>{mode === "dark" ? <LightModeIcon /> : <DarkModeIcon />}</Button>
-      </Box>
+      <Fade in={fadeIn} timeout={3000}>
+        <Box display={"flex"} justifyContent={"center"}>
+          <h1>Exchange Rates</h1>
+          <Button onClick={toggleColorMode}>{mode === "dark" ? <LightModeIcon /> : <DarkModeIcon />}</Button>
+        </Box>
+      </Fade>
 
-      <Box gap={2} m={1} display={"flex"} justifyContent={"center"}>
-        <TextField
-          id='startDate'
-          label='Start Date'
-          type='date'
-          value={startDate}
-          onChange={(e) => setStartDate(e.target.value)}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          inputProps={{
-            min: twoYearsAgo,
-            max: today,
-          }}
-        />
-        <TextField
-          id='endDate'
-          label='End Date'
-          type='date'
-          value={endDate}
-          onChange={(e) => setEndDate(e.target.value)}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          inputProps={{
-            min: twoYearsAgo,
-            max: today,
-          }}
-        />
-        <FormControl>
-          <InputLabel id='base-label'>From</InputLabel>
-          <Select value={baseCurrency} labelId='base-label' label='From' onChange={(e) => setBaseCurrency(e.target.value as string)}>
-            {currencies.map((currency) => (
-              <MenuItem key={currency} value={currency}>
-                {currency}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      </Box>
+      <Collapse in={fadeIn} timeout={1000}>
+        <Box gap={2} m={1} display={"flex"} justifyContent={"center"}>
+          <TextField
+            id='startDate'
+            label='Start Date'
+            type='date'
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            inputProps={{
+              min: twoYearsAgo,
+              max: today,
+            }}
+          />
+          <TextField
+            id='endDate'
+            label='End Date'
+            type='date'
+            value={endDate}
+            onChange={(e) => setEndDate(e.target.value)}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            inputProps={{
+              min: twoYearsAgo,
+              max: today,
+            }}
+          />
+          <FormControl>
+            <InputLabel id='base-label'>From</InputLabel>
+            <Select value={baseCurrency} labelId='base-label' label='From' onChange={(e) => setBaseCurrency(e.target.value as string)}>
+              {currencies.map((currency) => (
+                <MenuItem key={currency} value={currency}>
+                  {currency}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Box>
+      </Collapse>
 
-      <Box justifyContent={"center"} display={"flex"} m={3} gap={2}>
-        <Button variant='contained' onClick={() => setStartDate(getDateMonthsAgo(1))}>
-          1M
-        </Button>
-        <Button variant='contained' onClick={() => setStartDate(getDateMonthsAgo(3))}>
-          3M
-        </Button>
-        <Button variant='contained' onClick={() => setStartDate(getDateMonthsAgo(6))}>
-          6M
-        </Button>
-        <Button variant='contained' onClick={() => setStartDate(getDateYearsAgo(1))}>
-          1Y
-        </Button>
-        <Button variant='contained' onClick={() => setStartDate(twoYearsAgo)}>
-          2Y
-        </Button>
-      </Box>
+      <Collapse in={fadeIn} timeout={1000}>
+        <Box justifyContent={"center"} display={"flex"} m={3} gap={2}>
+          <Button variant='contained' onClick={() => setStartDate(getDateMonthsAgo(1))} className='shift-up'>
+            1M
+          </Button>
+          <Button variant='contained' onClick={() => setStartDate(getDateMonthsAgo(3))} className='shift-up'>
+            3M
+          </Button>
+          <Button variant='contained' onClick={() => setStartDate(getDateMonthsAgo(6))} className='shift-up'>
+            6M
+          </Button>
+          <Button variant='contained' onClick={() => setStartDate(getDateYearsAgo(1))} className='shift-up'>
+            1Y
+          </Button>
+          <Button variant='contained' onClick={() => setStartDate(twoYearsAgo)} className='shift-up'>
+            2Y
+          </Button>
+        </Box>
+      </Collapse>
 
       {loading ? (
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "50vh" }}>
